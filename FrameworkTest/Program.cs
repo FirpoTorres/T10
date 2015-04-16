f8f81cd7466b52e2753917381c9ac5f62570acd7
PabloSoligo2014 <psoligo@ing.unlam.edu.ar>
2015-04-16 19:22:35 -0300

    Ejemplo de namespace
    Ejemplo de manejos de namespace y clases para los alumnos de que tenga
    crear las primeras clases

--- FrameworkTest/Program.cs
+++ FrameworkTest/Program.cs
@@ -1,52 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Repository.Model;
 
 
 
 namespace FrameworkTest {
   class Program {
     static void Main(string[] args) {
+        Demo de = new Demo();
+        de.MyProperty = 1;
+        Console.WriteLine(de.MyProperty.ToString());
 
-      /*
-      ContextRepository cx = new ContextRepository(Properties.Settings.Default.ConnectionString);
-
-      List<Usuario> us = cx.GetUsuarios();
-
-      foreach(Usuario u in us){
-        Console.WriteLine("Usuario:" + u.Nombre);
-        
-      }
-
-      Console.WriteLine("-----------------------------------");
-
-      Usuario nu = new Usuario();
-      nu.Nombre = "peper";
-      nu.Password = "nada";
-      nu.Login = "peper";
-
-      cx.add(nu);
-
-      foreach (Usuario u in us) {
-        Console.WriteLine("Usuario:" + u.Nombre);
-
-      }
-
-      cx.Save();
-      cx.Delete(nu);
-      cx.Save();
-
-      Console.WriteLine("-----------------------------------");
-      foreach (Usuario u in us) {
-        Console.WriteLine("Usuario:" + u.Nombre);
-
-      }
-
-
-      Console.Read();
-
-
-      */
+        Console.ReadKey();
 
 
     }
---
+++ Repository/Model/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Model
+{
+    public class Demo
+    {
+
+        private int myProperty;
+
+        public int MyProperty {
+            get
+            {
+                return myProperty;
+                
+            }
+            set
+            {
+                myProperty = value;
+            }
+        
+        
+        }
+
+    }
+}
