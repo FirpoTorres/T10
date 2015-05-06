78577ce23b919cae04e098e27fbc6482844e8f10
DanielaPalotti <danypalotti@gmail.com>
2015-05-06 18:33:07 -0300

    Clase seleccion

---
+++ Repository/Model/Clase seleccion.cs
@@ -0,0 +1,39 @@
+﻿	using System;
+	using System.Collections.Generic;
+	using System.Text;
+
+namespace Repository.Model
+{
+    class Seleccion
+    {
+        private string descripcion;
+
+        public void SetDescripcion(string value)
+        {
+            if (value.Length >= 256)
+            {
+                descripcion = value.Substring(0, 256);
+            }
+            else
+            {
+                descripcion = value;
+            }
+        }
+
+        public string GetDescripcion()
+        {
+            return descripcion;
+        }
+
+
+        private int idSeleccion;
+
+        public int IDseleccion
+        {
+            get { return idSeleccion; }
+            set { idSeleccion = value; }
+        }
+
+    }
+
+}
