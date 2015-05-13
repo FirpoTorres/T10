5050f8942d9685ba29cd7cbaf1de56d187625080
LeandroAc <acostaleandrod@gmail.com>
2015-05-13 20:48:12 -0300

    Agregada clase elementoSeleccion
    Atributos:
    
    - Int IdElementoSeleccion.
    - Int IdSeleccion.
    - String descripcion.

---
+++ Repository/Model/elementoSeleccion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1.Clase
+{
+    class elementoSeleccion
+    {
+        
+        private int IdElementoSeleccion;
+        private int IdSeleccion;
+        private string descripcion;
+
+        public int idElementoSeleccion
+        {
+            get
+            {
+                return IdElementoSeleccion;
+            }
+
+            set
+            {
+                IdElementoSeleccion = value;
+            }
+        }
+
+        
+        public int idSeleccion
+        {
+            get
+            {
+                return IdSeleccion;
+            }
+
+            set
+            {
+                IdSeleccion = value;
+            }
+
+        }
+
+        
+        public string Descripcion
+        {
+            
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    descripcion = value;
+                }
+                else
+                {
+        }
+
+    }    
+
+}
+
