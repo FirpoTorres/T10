77f946cfcabcd5db6cc3e245db0ccc620bce2ef5
erik940 <erik.aguilera94@gmail.com>
2015-05-14 13:27:43 -0300

    Create Encuesta.cs

---
+++ Repository/Model/Encuesta.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Encuesta
+    {
+        private int idEncuesta;
+        private DateTime fechaHora;
+        private int idTipoEncuesta;
+
+        
+
+
+        public int Id_encuesta
+        {
+            get
+            {
+                return idEncuesta;
+            }
+            set {   
+                idEncuesta = value;    
+            }
+        }
+
+        public int Id_tipo_encuesta
+        {
+            get
+            {
+                return idTipoEncuesta;
+            }
+            set
+            {
+                idTipoEncuesta = value;
+            }
+        }
+
+        public DateTime Fecha_hora
+        {
+            get
+            {
+                return fechaHora; 
+            }
+            set
+            {
+                fechaHora = DateTime.UtcNow; 
+            }
+        }
+    
+    
+    
+    }
+}
