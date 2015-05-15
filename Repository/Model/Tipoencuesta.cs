9103b0c590f3c2296d1129d223f3b402dd444928
ricolanzieri <diego.rico.dk@gmail.com>
2015-05-15 16:57:24 -0300

    Create Tipoencuesta.cs

---
+++ Repository/Model/Tipoencuesta.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+
+namespace Repository.Model
+{
+    public class Tipoencuesta
+    {
+        private int idtipoencuesta;
+        private string nombre;
+
+        /*--- PROPIEDADES ---*/
+
+        public int Idtipoencuesta
+        {
+            get
+            {
+                return idtipoencuesta; 
+                
+            }
+            set
+            {
+                idtipoencuesta = value; 
+            }
+
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre; 
+            }
+
+            set
+            {
+                if (value.Length > 128)
+                {
+                    nombre = value.Substring(0,127);
+                }
+                else
+                {
+                    nombre = value; 
+                }
+            }
+        }
+    }
+}
