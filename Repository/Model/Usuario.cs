a25ec3361746337d758a177b850d4d833b171395
MicaLucas <lucasq95@hotmail.com.ar>
2015-05-29 20:20:17 -0300

    Clase usuario
    Modificaciòn. Lo pasamos a propiedades

--- Repository/Model/Usuario.cs
+++ Repository/Model/Usuario.cs
@@ -4,53 +4,87 @@ using System.Text;
 
 namespace Repository.Model
 {
-    public class Usuario
+    class Usuario
     {
         private string nombre;
-
-        public void SetNombre(string value){ /*crear el nombre del usuario*/
-            if (value.Length >= 128){ /*si tiene mas de 128 caracteres imprime un mensaje de error*/
-                nombre = value.Substring(0, 128);
-            }else{
-                nombre = value; 
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
             }
-
-
-
+            set
+            {
+                if (value.Length >= 128)
+                {
+                    nombre = value.Substring(0, 128);
+                }
+                else
+                {
+                    nombre = value;
+                }
+            }
         }
-
-        public string GetNombre(){ /*devolver el nombre del usuario*/
-            return nombre;}
-
-
         private string login;
-        public void SetLogin(string value){ /*crear el login del usuario*/
-            if (value.Length >= 20){ /*si es mayor a 20 caracteres imprime un mensaje de error*/
-                login = value.Substring(4, 20);
-            }else{
-                login = value; 
+        public string Login
+        {
+            get
+            {
+                return login;
             }
-        }
+            set
+            {
+                if (value.Length >= 20)
+                {
+                    login = value.Substring(4, 20);
+                }
+                else
+                {
+                    login = value;
+                }
+            }
 
-        public string GetLogin()
-        { /*devuelve el login del usuario*/
-            return login;
         }
-
 
         private string password;
-        
-        public void SetPassword(string value)
-        { /*crear el password del usuario*/
-            if (value.Length >= 20)
-            { /*si el password es mayor a 20 caracteres imprime un mensaje de error*/
-                password = value.Substring(4, 20);
-            }
-            else
+        public string Password
+        {
+            //get { }
+            set
             {
-                password = value; 
+                if (value.Length >= 20)
+                {
+                    password = value.Substring(4, 20);
+                }
+                else
+                {
+                    password = value;
+                }
             }
-        }//PABLO SOLIGO: No subir codigo que no compila por favor!
+
+        }
+
+        private string ClaveInterna;
+        private string SimEncrypt(string password)
+        {
+            StringBuilder inSb = new StringBuilder(password);
+            StringBuilder outSb = new StringBuilder(password.Length);
+            char c;
+            ClaveInterna = "aaaa";
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                c = inSb[i];
+                c = (char)(c ^ (char)ClaveInterna[(i + 4) % 4]);
+                outSb.Append(c);
+            }
+            return outSb.ToString();
+        }
+
+    }
+}
+    
+  //PABLO SOLIGO: No subir codigo que no compila por favor!
 
         /* PABLO SOLIGO : Reparar esto no lo quiero hacer yo
          * Modifico codigo solo para compile 
@@ -75,8 +109,7 @@ namespace Repository.Model
         }
         */
     
-    }
 
-}
+
 
 
