401728c7ad2c6c5b939fa5b7f54ed0909b0cfd75
PabloSoligo2014 <psoligo@ing.unlam.edu.ar>
2015-05-30 11:13:21 -0300

    Get Usuario en context repository
    Por no cumplimiento de grupo.

--- Repository/ContextRepository.cs
+++ Repository/ContextRepository.cs
@@ -6,31 +6,48 @@ using System.Data.OleDb;
 using Repository.Model;
 
 namespace Repository {
- 
 
 
-  public class ContextRepository {
-    private String ConnectionString;
 
-    public ContextRepository(String cs) {
-      ConnectionString = cs;
-    
+    public class ContextRepository {
+        private String ConnectionString;
+
+        public ContextRepository(String cs) {
+            ConnectionString = cs;
+
+        }
+
+
+        public List<Usuario> GetUsuarios() {
+            return null;
+        }
+
+
+
+        public Usuario NuevoUsuario() {
+            return null;
+        }
+
+        public Usuario GetUsuario(String Login) {
+            OleDbConnection conn = new OleDbConnection(this.ConnectionString);
+            conn.Open();
+            OleDbCommand comm = new OleDbCommand("Select * from Usuario Where Login=@Login", conn);
+            comm.Parameters.AddWithValue("@Login", Login);
+
+            OleDbDataReader dr = comm.ExecuteReader();
+            if (dr.HasRows) {
+                dr.Read();
+                Usuario Result = new Usuario();
+
+
+                Result.Login = dr["Login"].ToString();
+                Result.Password = dr["Password"].ToString();
+                Result.Nombre = dr["Nombre"].ToString();
+
+                return Result;
+            }
+            return null;
+
+        }
     }
-
- 
-    public List<Usuario> GetUsuarios() {
-        return null;  
-    }
-
-    
-
-    public Usuario NuevoUsuario() {
-        return null;
-    }
-
-    public Usuario GetUsuario(String Login) {
-        return null;
-    }
-
-  }
 }
